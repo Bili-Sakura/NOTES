4b87abd02f3ac70594042db423238cd5368933cd
ChenZhenYuan319 <3190100890@zju.edu.cn>
2024-06-16 22:41:26 +0800

    update

---
+++ home_page.py
@@ -0,0 +1,18 @@
+import os
+import streamlit as st
+
+
+st.set_page_config(page_title="Sakura Notes Home Page", page_icon=None, layout="wide")
+st.title("Sakura Notes Home Page")
+st.image("./assets/sakura.png")
+# tab_profile, tab_llm_0, tab_llm_1 = st.tabs(["Profile", "LLM_0", "LLM_1"])
+
+
+# with tab_llm_0:
+#     with open(
+#         "./Bilibili_Course/LLM/course_notes/Markdown/【序】五分钟了解大语言模型.md",
+#         "r",
+#         encoding="utf-8",
+#     ) as f:
+#         content = f.read()
+#         st.markdown(content, unsafe_allow_html=True)
