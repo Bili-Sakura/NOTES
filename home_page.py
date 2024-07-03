7fd77a4f0619e4aec03a4fb108d5e75f0bd331a2
ChenZhenYuan319 <3190100890@zju.edu.cn>
2024-07-03 18:28:44 +0800

    update

--- home_page.py
+++ home_page.py
@@ -18,26 +18,49 @@ st.image("./assets/sakura00.png")
 #         content = f.read()
 #         st.markdown(content, unsafe_allow_html=True)
 
-# LangChain supports many other chat models. Here, we're using Ollama
-from langchain_community.chat_models import ChatOllama
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.globals import set_verbose, get_verbose
+# # LangChain supports many other chat models. Here, we're using Ollama
+# from langchain_community.chat_models import ChatOllama
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain.globals import set_verbose, get_verbose
 
-# Example usage
-set_verbose(value=True)  # Enable verbose mode
+# # Example usage
+# set_verbose(value=True)  # Enable verbose mode
 
-# supports many more optional parameters. Hover on your `ChatOllama(...)`
-# class to view the latest available supported parameters
-llm = ChatOllama(model="llama3")
-prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
+# # supports many more optional parameters. Hover on your `ChatOllama(...)`
+# # class to view the latest available supported parameters
+# llm = ChatOllama(model="llama3")
+# prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
 
-# using LangChain Expressive Language chain syntax
-# learn more about the LCEL on
-# /docs/concepts/#langchain-expression-language-lcel
-chain = prompt | llm | StrOutputParser()
+# # using LangChain Expressive Language chain syntax
+# # learn more about the LCEL on
+# # /docs/concepts/#langchain-expression-language-lcel
+# chain = prompt | llm | StrOutputParser()
 
-# for brevity, response is printed in terminal
-# You can use LangServe to deploy your application for
-# production
-print(chain.invoke({"topic": "Space travel"}))
+# # for brevity, response is printed in terminal
+# # You can use LangServe to deploy your application for
+# # production
+# print(chain.invoke({"topic": "Space travel"}))
+
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate.from_template(template)
+
+llm = OpenAI(model_name="gpt-3.5-turbo-instruct")
+
+llm_chain = prompt | llm
+
+question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+
+answer = llm_chain.invoke(question)
+
+print(answer)
